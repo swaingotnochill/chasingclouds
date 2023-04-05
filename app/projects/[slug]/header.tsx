42ad03480b73f2b8ddff43a9799b2e088fe25c92
Roshan Swain <swainroshan001@gmail.com>
2023-04-05 18:51:00 +0530

    clean: minor cleaning up

--- app/projects/[slug]/header.tsx
+++ app/projects/[slug]/header.tsx
@@ -67,7 +67,7 @@ export const Header: React.FC<Props> = ({ project, views }) => {
                 views
               )}
             </span>
-            <Link target="_blank" href="https://twitter/pucchkaa">
+            <Link target="_blank" href="https://twitter.com/pucchkaa">
               <Twitter
                 className={`w-6 h-6 duration-200 hover:font-medium ${
                   isIntersecting
